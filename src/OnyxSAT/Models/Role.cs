975a0c9fb099e4bb462f6aa0b5dc438576db4f4e
Josiah Sonsie <101094376@student.swin.edu.au>
2017-08-22 15:46:51 +1000

    Copied work from school

--- src/OnyxSAT/Models/Role.cs
+++ src/OnyxSAT/Models/Role.cs
@@ -5,6 +5,12 @@ namespace OnyxSAT.Models
 {
     public class Role
     {
-
+        public int RoleId { get; set; }
+		[StringLength(10)]
+		public string Name { get; set; }
+		[StringLength(100)]
+		public string Descriptions { get; set; }
+		
+		public ICollection<UserRole> UserRoles { get; set; }
     }
 }
