b259c6684feb3a368d00d48e84c8ea8a66d57bab
Simon Lee <slee1994@hotmail.com>
2017-08-22 13:18:54 +1000

    Model changes
    Added relationships between Card and Attendance models.

--- src/OnyxSAT/Models/Attendance.cs
+++ src/OnyxSAT/Models/Attendance.cs
@@ -1,11 +1,18 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace OnyxSAT.Models
 {
     public class Attendance
     {
+        [Key]
 
+        [StringLength(6)]
+        public string DateTime { get; set; }
 
+        public Boolean Verified { get; set; }
+
+        //public virtual ICollection<Card> Cards { get; set; }
     }
 }
--- src/OnyxSAT/Models/Card.cs
+++ src/OnyxSAT/Models/Card.cs
@@ -1,10 +1,17 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace OnyxSAT.Models
 {
     public class Card
     {
+    //Primary Key
+     [StringLength(16)]
 
+     [Key]
+     public string CardNo { get; set;}
+     public string DateTimeID { get; set;}
+     public virtual ICollection<Attendance> Attendances { get; set; }
     }
 }
