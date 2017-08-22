8cf60ec88bbb0b6943e943f2aa3a33d88c27bdb1
liambailey96 <30608900+liambailey96@users.noreply.github.com>
2017-08-22 15:46:30 +1000

    This should fix

--- src/OnyxSAT/Models/User.cs
+++ src/OnyxSAT/Models/User.cs
@@ -5,6 +5,23 @@ namespace OnyxSAT.Models
 {
     public class User
     {
+        //this should work
+        public int userId { get; set; }
+        [StringLength(50)]
+        public string firstName { get; set; }
+        [StringLength(50)]
+        public string lastName { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public date dateOfBirth { get; set; }
+        [StringLength(10)]
+        public string gender { get; set; }
+        public int StudentId { get; set; }
+        public int TeacherId { get; set; }
+        public int mobile { get; set; }
+        [StringLength(50)]
+        email string { get; set; } 
+
+        public ICollection<UserRole> UserRoles { get; set; }
 
     }
 }
