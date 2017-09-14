19d6685ba95ec5a4ef24b00a95f8a1e7b172be14
Kyle Sutton <101122659@student.swin.edu.au>
2017-09-14 10:28:04 +1000

    Optimised Home css and changed verification for add user form
    Added validation for first name and last name and changed formating of home page.

--- src/OnyxSAT/ClientApp/components/Home.vue
+++ src/OnyxSAT/ClientApp/components/Home.vue
@@ -1,43 +1,32 @@
 <template>
-    <div style="text-align:center" class="home align-items-center container">
-        <h1>ONYX</h1>
-        <h2>Attendence Tracker</h2>
+    <div class="home align-items-center container">
+        <h1 class="display-3">ONYX</h1>
+        <h2 class="">Attendence Tracker</h2>
         <h3>Swinburne University</h3>
         <h4>Developed by ONYX</h4>
     </div>
 </template>
-<style scoped> 
-h1 {
+<style scoped>
+.home {
     text-align: center;
-    color:black;
-    font-family: Book Antiqua;
 }
 h2 {
-    text-align: center;
     color: gray;
     font-size: 64px;
     display: inline;
-    font-family: Book Antiqua;
-}
-h4 {
-    text-align: center;
-    color: black;
-    font-size: 18px;
-    margin-top: 180px;
 }
 h3 { 
-    text-align: center;
     color: darkgrey;
     font-size: 48px;
     margin-top: 24px;
-    font-family: Book Antiqua;
+}
+h4 {
+    font-size: 18px;
+    margin-top: 180px;
 }
 h5 {
-    text-align: center;
-    color:black;
     font-size: 78px;
     margin-top: 24px;
     margin-bottom: 24px;
-    font-family: Book Antique;
 }
 </style>
--- src/OnyxSAT/ClientApp/components/users/AddUser.vue
+++ src/OnyxSAT/ClientApp/components/users/AddUser.vue
@@ -1,29 +1,31 @@
+
+
 <template>
   <div class="d-flex flex-column align-items-center container">
-    <h1>Add a User</h1>
-    <ul v-if="errors.any()">
+    <h1 class="display-4">Add a User</h1>
+    <!-- <ul v-if="errors.any()">
       <li v-for="error in errors.all()"> {{ error }}</li>
-    </ul>
+    </ul> -->
     <section class="row w-100 d-flex align-items-center flex-column">
       <form @submit.prevent="addUser" class="d-flex flex-column w-100 mb-3">
         <fieldset class="form-group">
           <label>First Name</label>
-          <input type="text" name="firstName" class="form-control" v-validate="'required'" v-model="user.firstName" />
+          <input type="text" name="First Name" class="form-control" v-validate="{required: true, alpha_spaces: true}" v-model="user.firstName" />
         </fieldset>
         <fieldset class="form-group">
           <label>Last Name</label>
-          <input type="text" name="lastName" class="form-control" v-validate="'required'" v-model="user.lastName" />
+          <input type="text" name="Last Name" class="form-control" v-validate="{required: true, alpha_spaces: true}" v-model="user.lastName" />
         </fieldset>
         <fieldset class="form-group">
           <label>Email</label>
-          <input type="email" name="email" class="form-control" v-validate="'required'" v-model="user.email" />
+          <input type="email" name="Email" class="form-control" v-validate="'required'" v-model="user.email" />
         </fieldset>
         <fieldset class="form-group">
           <label>Mobile</label>
-          <input type="text" name="mobile" class="form-control" v-validate="{ rules: { required: true, regex: /^04\d{8}$/} }" v-model="user.mobile" />
+          <input type="text" name="Mobile" class="form-control" v-validate="{ rules: { required: true, regex: /^04\d{8}$/} }" v-model="user.mobile" />
         </fieldset>
         <br>
-        <button type="submit" class="btn btn-default">Submit</button>
+        <button type="submit" class="submit btn btn-default">Submit</button>
       </form>
     </section>
   </div>
@@ -55,4 +57,5 @@ export default {
 form
   max-width: 500px
   text-align: center
+  display: inline
 </style>
