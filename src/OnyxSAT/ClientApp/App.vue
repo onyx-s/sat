9b47060dfaae6970878a36480753b030e5c446d2
Kyle Sutton <101122659@student.swin.edu.au>
2017-10-31 11:45:59 +1100

    Added loading screen. Not currently functional

--- src/OnyxSAT/ClientApp/App.vue
+++ src/OnyxSAT/ClientApp/App.vue
@@ -1,15 +1,18 @@
 <template>
     <div id='app' class="container-fluid">
         <nav-bar></nav-bar>
-        <router-view></router-view>        
+        <router-view></router-view>   
+        <loading></loading>     
     </div>
 </template>
 
 <script>
 import Vue from 'vue'
 import NavBar from './components/NavBar'
+import Loading from './components/Loading'
 
 Vue.component('nav-bar', NavBar)
+Vue.component('loading', Loading)
 
 export default {
   name: 'app',
